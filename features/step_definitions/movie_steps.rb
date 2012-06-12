99f77593cbe1a6c78fc32b1fb8efb383be09521e
Bernd Jungblut <bernd.jungblut@googlemail.com>
2012-06-12 13:59:47 -0700

    check e1 befor e2

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,3 +1,4 @@
+
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
@@ -11,7 +12,6 @@ Given /the following movies exist/ do |movies_table|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-#  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -20,7 +20,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.content.match(/#{e1}.*#{e2}/)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
